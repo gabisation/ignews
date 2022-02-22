7b407a255211c90e29e15e48af1e3c9f328aefff
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2022-02-22 00:47:00 -0300

    Listening more events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,10 @@ export const config = {
 }
 
 const releventEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ])
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
@@ -44,12 +47,24 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     if (releventEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+          
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+
+            break
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
 
             break
